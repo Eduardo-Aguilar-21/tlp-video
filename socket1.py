d4bc2fef3887dabcd7ed3fffffe199946ab8913e
Eduardo Aguilar <es123y123@gmail.com>
2025-07-01 17:04:25 -0500

    c

--- socket1.py
+++ socket1.py
@@ -1,7 +1,7 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 6601))
+server_socket.bind(('0.0.0.0', 9000))
 server_socket.listen(1)
 print("Escuchando en el puerto 200...")
 
